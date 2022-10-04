c62073926904785d9415f924880c098b96c8920e
Alexandra Mallet <malletalexandra@gmail.com>
2022-10-04 10:23:56 +0200

    fichier app

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
+const postRoutes = require("./routes/post");
+const dotenv = require("dotenv").config();
+const path = require("path");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use(helmet({crossOriginResourcePolicy: false}));
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+	max: 100, 
+	standardHeaders: true,
+	legacyHeaders: false,
+});
+
+app.use(limiter);
+
+
+mongoose.connect(process.env.URL_DATABASE,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then(() => console.log("Application connectée à MongoDB."))
+    .catch(() => console.log("Impossible de se connecter à MongoDB.")); 
+  
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+
+app.use("/api/auth", userRoutes);
+app.use("/api/post", postRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+
+module.exports = app;
