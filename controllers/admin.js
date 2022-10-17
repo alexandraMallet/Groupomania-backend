31d9366ad2b13ee4d75030a3a80d0bf64246353a
Alexandra Mallet <malletalexandra@gmail.com>
2022-10-17 14:13:07 +0200

    admin routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -41,3 +41,23 @@ exports.signup = (req, res, next) => {
                 .catch ((error) => res.status(500).json({ error }))})
     .catch ((error) => res.status(500).json({ error }));
 };
+
+
+exports.login = (req, res, next) => {
+
+};
+
+
+exports.modify = (req, res, next) => {
+
+};
+
+
+exports.delete = (req, res, next) => {
+
+};
+
+
+exports.getAdmin = (req, res, next) => {
+
+};
