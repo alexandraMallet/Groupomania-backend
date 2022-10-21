4b0214b6181500e3ee9315058547f9d7024207ff
Alexandra Mallet <malletalexandra@gmail.com>
2022-10-21 10:29:47 +0200

    ctr getAllPosts

--- controllers/post.js
+++ controllers/post.js
@@ -1,6 +1,6 @@
 const Post = require("../models/Post");
 const User = require("../models/User");
-const { unsubscribe } = require("../routes/user");
+// const { unsubscribe } = require("../routes/user");
 
 
 exports.createPost = (req, res, next) => {
@@ -8,18 +8,18 @@ exports.createPost = (req, res, next) => {
         userId: req.auth.userId,
         text: req.body.text,
         imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
-        //createdAt: `${Date.now}`
+        createdAt: `${Date.now()}`
     });
     post.save()
         .then(() => res.status(201).json({ message: `Nouvelle publication : ${post.text}` }))
-        .catch((error) => res.status(400).json({ error }));
+       // .catch((error) => res.status(400).json({ error }));
 
     User.findOne({ _id: req.auth.userId })
         .then(user => {
             user.posts.push(`${post._id}`);
             user.save();
         })
-        .catch((error) => res.status(400).json({ error }));
+       // .catch((error) => res.status(400).json({ error }));
 };
 
 
@@ -32,7 +32,9 @@ exports.getAllPosts = (req, res, next) => {
 
 
 exports.getOnePost = (req, res, next) => {
-
+    Post.findById(req.params.id)
+    .then(post => res.status(200).json(post))
+    .catch((error) => res.status(404).json({error}));
 };
 
 
