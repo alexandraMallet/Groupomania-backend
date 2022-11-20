44120a77d58a46ad5576132ced67929fcacc3778
Alexandra Mallet <malletalexandra@gmail.com>
2022-11-20 09:11:04 +0100

    commentaires

--- routes/post.js
+++ routes/post.js
@@ -4,11 +4,11 @@ const postCtr = require("../controllers/post");
 const multer = require("../middleware/multer-config");
 const auth = require("../middleware/auth");
 
-router.post("/", auth, multer, postCtr.createPost);    
-router.get("/", auth, postCtr.getAllPosts);
-router.get("/:id", auth, postCtr.getOnePost);
-router.put("/:id", auth, multer, postCtr.modifyPost);
-router.delete("/:id", auth, multer, postCtr.deletePost);
-router.post("/:id/like", auth, postCtr.likeDislikePost);
+router.post("/", auth, multer, postCtr.createPost);    //front ok
+router.get("/", auth, postCtr.getAllPosts);             //front ok
+router.get("/:id", auth, postCtr.getOnePost);            //front ok
+router.put("/:id", auth, multer, postCtr.modifyPost);      //front ok
+router.delete("/:id", auth, multer, postCtr.deletePost);      //front ok
+router.post("/:id/like", auth, postCtr.likeDislikePost);      // front TODO
 
 module.exports = router;
